66d3f8655d45e3f67b102e8944b0a1e5526a0cea
v.golikov <v.golikov@rdp.ru>
2024-04-21 16:12:37 +0300

    add some files

---
+++ tests/test_web.py
@@ -0,0 +1,14 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+
+def test_login_page(browser):
+    browser.get(browser.url + "/administration")
+    #time.sleep(100)
+    browser.find_element(By.ID, "input-username")
+    browser.find_element(By.NAME, "password")
+    browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    browser.find_element(By.LINK_TEXT, "Forgotten Password")
+    browser.find_element(By.XPATH, "//*[text()='OpenCart']")
+    time.sleep(100) # Для демонстрации
